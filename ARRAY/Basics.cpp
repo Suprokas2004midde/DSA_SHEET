07b322701ff427f864682bc84d76844778f61100
Suprokas Midde <suprokas2004midde@gmail.com>
2024-06-21 21:00:38 +0530

    second update

--- ARRAY/Basics.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    int size=10;
-    int b[size];
-    return 0;
-}
--- FUNCTION/Pascal-Triangle.c
+++ FUNCTION/Pascal-Triangle.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int fact(int a)
+int fact(int a)  /// determinees only the factorial.............
 {
     int sum=1;
     for(int i=1;i<=a;i++)
@@ -8,7 +8,7 @@ int fact(int a)
     }
     return sum;
 }
-int result(int a,int b)
+int result(int a,int b)  // determines the value of combination of the giveen row and column...
 {
     int result=fact(a)/(fact(b)*fact(a-b));
     return result;
@@ -20,13 +20,13 @@ int main()
     scanf("%d",&n);
     for(int i=0;i<=n;i++)
     {
-        for(int k=n-i;k>0;k--)
+        for(int k=n-i;k>0;k--) //prints thhe blanks space......
         {
             printf(" ");
         }
         for(int j=0;j<=i;j++)
         {
-            ncr=result(i,j);
+            ncr=result(i,j); // pases thhe value of row and column....
             printf("%d ",ncr);
         }
         printf("\n");
